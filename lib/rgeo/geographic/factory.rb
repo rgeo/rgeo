223540ff4b2dd4a1dc17b925ee8e1d365fca1341
Kashif Rasul <kashif.rasul@gmail.com>
2013-06-19 15:53:48 +0200

    fix for dazuma/rgeo/issues/54

--- lib/rgeo/geographic/factory.rb
+++ lib/rgeo/geographic/factory.rb
@@ -301,7 +301,7 @@ module RGeo
 
       def unproject(geometry_)
         return nil unless geometry_
-        unless @projector && @projector.projection_factory == geometry_.factory
+        unless @projector && @projector.projection_factory.srid == geometry_.factory.srid && @projector.projection_factory.proj4 == geometry_.factory.proj4
           raise Error::InvalidGeometry, 'You can unproject only features that are in the projected coordinate space.'
         end
         @projector.unproject(geometry_)
