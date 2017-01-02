2f03558b1302f141fe0689545b8ab6c428bcc3ff
Tee Parham <tee@neighborland.com>
2017-01-02 10:37:30 -0700

    Cleanup test file
    * Use test_helper
    * Use refute
    * Less parens
    * Remove :: namespace qualifiers

--- test/types_test.rb
+++ test/types_test.rb
@@ -4,32 +4,31 @@
 #
 # -----------------------------------------------------------------------------
 
-require "test/unit"
-require "rgeo"
+require "test_helper"
 
 class TestTypes < Test::Unit::TestCase
   def test_geometry
-    assert_equal("Geometry", ::RGeo::Feature::Geometry.type_name)
-    assert_nil(::RGeo::Feature::Geometry.supertype)
-    assert(::RGeo::Feature::Geometry.subtype_of?(::RGeo::Feature::Geometry))
-    assert(!::RGeo::Feature::Geometry.subtype_of?(::RGeo::Feature::Point))
+    assert_equal "Geometry", RGeo::Feature::Geometry.type_name
+    assert_nil RGeo::Feature::Geometry.supertype
+    assert RGeo::Feature::Geometry.subtype_of?(RGeo::Feature::Geometry)
+    refute RGeo::Feature::Geometry.subtype_of?(RGeo::Feature::Point)
   end
 
   def test_point
-    assert_equal("Point", ::RGeo::Feature::Point.type_name)
-    assert_equal(::RGeo::Feature::Geometry, ::RGeo::Feature::Point.supertype)
-    assert(::RGeo::Feature::Point.subtype_of?(::RGeo::Feature::Point))
-    assert(::RGeo::Feature::Point.subtype_of?(::RGeo::Feature::Geometry))
-    assert(!::RGeo::Feature::Point.subtype_of?(::RGeo::Feature::LineString))
+    assert_equal "Point", RGeo::Feature::Point.type_name
+    assert_equal RGeo::Feature::Geometry, RGeo::Feature::Point.supertype
+    assert RGeo::Feature::Point.subtype_of?(RGeo::Feature::Point)
+    assert RGeo::Feature::Point.subtype_of?(RGeo::Feature::Geometry)
+    refute RGeo::Feature::Point.subtype_of?(RGeo::Feature::LineString)
   end
 
   def test_line_string
-    assert_equal("LineString", ::RGeo::Feature::LineString.type_name)
-    assert_equal(::RGeo::Feature::Curve, ::RGeo::Feature::LineString.supertype)
-    assert(::RGeo::Feature::LineString.subtype_of?(::RGeo::Feature::LineString))
-    assert(::RGeo::Feature::LineString.subtype_of?(::RGeo::Feature::Curve))
-    assert(::RGeo::Feature::LineString.subtype_of?(::RGeo::Feature::Geometry))
-    assert(!::RGeo::Feature::LineString.subtype_of?(::RGeo::Feature::Line))
+    assert_equal "LineString", RGeo::Feature::LineString.type_name
+    assert_equal RGeo::Feature::Curve, RGeo::Feature::LineString.supertype
+    assert RGeo::Feature::LineString.subtype_of?(RGeo::Feature::LineString)
+    assert RGeo::Feature::LineString.subtype_of?(RGeo::Feature::Curve)
+    assert RGeo::Feature::LineString.subtype_of?(RGeo::Feature::Geometry)
+    refute RGeo::Feature::LineString.subtype_of?(RGeo::Feature::Line)
   end
 
   def test_illegal_cast
