bfcd6cfeeeff67c7f35a09b59f9ed64d2289b2ad
Tee Parham <tee@neighborland.com>
2017-12-31 16:19:45 -0700

    Remove tests from modules

--- test/cartesian_analysis_test.rb
+++ test/cartesian_analysis_test.rb
@@ -4,62 +4,57 @@
 #
 # -----------------------------------------------------------------------------
 
-require "test/unit"
-require "rgeo"
+require "test_helper"
 
-module RGeo
-  module Tests # :nodoc:
-    class TestCartesianAnalysis < ::Test::Unit::TestCase # :nodoc:
-      def setup
-        @factory = ::RGeo::Cartesian.simple_factory
-      end
+class TestCartesianAnalysis < Test::Unit::TestCase # :nodoc:
+  def setup
+    @factory = ::RGeo::Cartesian.simple_factory
+  end
 
-      def test_ring_direction_clockwise_triangle
-        p1_ = @factory.point(1, 1)
-        p2_ = @factory.point(2, 4)
-        p3_ = @factory.point(5, 2)
-        ring_ = @factory.line_string([p1_, p2_, p3_, p1_])
-        assert_equal(-1, ::RGeo::Cartesian::Analysis.ring_direction(ring_))
-      end
+  def test_ring_direction_clockwise_triangle
+    p1_ = @factory.point(1, 1)
+    p2_ = @factory.point(2, 4)
+    p3_ = @factory.point(5, 2)
+    ring_ = @factory.line_string([p1_, p2_, p3_, p1_])
+    assert_equal(-1, ::RGeo::Cartesian::Analysis.ring_direction(ring_))
+  end
 
-      def test_ring_direction_counterclockwise_triangle
-        p1_ = @factory.point(1, 1)
-        p2_ = @factory.point(2, 4)
-        p3_ = @factory.point(5, 2)
-        ring_ = @factory.line_string([p1_, p3_, p2_, p1_])
-        assert_equal(1, ::RGeo::Cartesian::Analysis.ring_direction(ring_))
-      end
+  def test_ring_direction_counterclockwise_triangle
+    p1_ = @factory.point(1, 1)
+    p2_ = @factory.point(2, 4)
+    p3_ = @factory.point(5, 2)
+    ring_ = @factory.line_string([p1_, p3_, p2_, p1_])
+    assert_equal(1, ::RGeo::Cartesian::Analysis.ring_direction(ring_))
+  end
 
-      def test_ring_direction_clockwise_puckered_quad
-        p1_ = @factory.point(1, 1)
-        p2_ = @factory.point(2, 6)
-        p3_ = @factory.point(3, 3)
-        p4_ = @factory.point(5, 2)
-        ring_ = @factory.line_string([p1_, p2_, p3_, p4_, p1_])
-        assert_equal(-1, ::RGeo::Cartesian::Analysis.ring_direction(ring_))
-      end
+  def test_ring_direction_clockwise_puckered_quad
+    p1_ = @factory.point(1, 1)
+    p2_ = @factory.point(2, 6)
+    p3_ = @factory.point(3, 3)
+    p4_ = @factory.point(5, 2)
+    ring_ = @factory.line_string([p1_, p2_, p3_, p4_, p1_])
+    assert_equal(-1, ::RGeo::Cartesian::Analysis.ring_direction(ring_))
+  end
 
-      def test_ring_direction_counterclockwise_puckered_quad
-        p1_ = @factory.point(1, 1)
-        p2_ = @factory.point(2, 6)
-        p3_ = @factory.point(3, 3)
-        p4_ = @factory.point(5, 2)
-        ring_ = @factory.line_string([p1_, p4_, p3_, p2_, p1_])
-        assert_equal(1, ::RGeo::Cartesian::Analysis.ring_direction(ring_))
-      end
+  def test_ring_direction_counterclockwise_puckered_quad
+    p1_ = @factory.point(1, 1)
+    p2_ = @factory.point(2, 6)
+    p3_ = @factory.point(3, 3)
+    p4_ = @factory.point(5, 2)
+    ring_ = @factory.line_string([p1_, p4_, p3_, p2_, p1_])
+    assert_equal(1, ::RGeo::Cartesian::Analysis.ring_direction(ring_))
+  end
 
-      def test_ring_direction_counterclockwise_near_circle
-        p1_ = @factory.point(0, -3)
-        p2_ = @factory.point(2, -2)
-        p3_ = @factory.point(3, 0)
-        p4_ = @factory.point(2, 2)
-        p5_ = @factory.point(0, 3)
-        p6_ = @factory.point(-2, 2)
-        p7_ = @factory.point(-3, 0)
-        p8_ = @factory.point(-2, -2)
-        ring_ = @factory.line_string([p1_, p2_, p3_, p4_, p5_, p6_, p7_, p8_, p1_])
-        assert_equal(1, ::RGeo::Cartesian::Analysis.ring_direction(ring_))
-      end
-    end
+  def test_ring_direction_counterclockwise_near_circle
+    p1_ = @factory.point(0, -3)
+    p2_ = @factory.point(2, -2)
+    p3_ = @factory.point(3, 0)
+    p4_ = @factory.point(2, 2)
+    p5_ = @factory.point(0, 3)
+    p6_ = @factory.point(-2, 2)
+    p7_ = @factory.point(-3, 0)
+    p8_ = @factory.point(-2, -2)
+    ring_ = @factory.line_string([p1_, p2_, p3_, p4_, p5_, p6_, p7_, p8_, p1_])
+    assert_equal(1, ::RGeo::Cartesian::Analysis.ring_direction(ring_))
   end
 end
--- test/mixins_test.rb
+++ test/mixins_test.rb
@@ -4,138 +4,133 @@
 #
 # -----------------------------------------------------------------------------
 
-require "test/unit"
-require "rgeo"
+require "test_helper"
 
-module RGeo
-  module Tests # :nodoc:
-    class TestMixins < ::Test::Unit::TestCase # :nodoc:
-      module Mixin1  # :nodoc:
-        def mixin1_method
-        end
-      end
-
-      module Mixin2  # :nodoc:
-        def mixin2_method
-        end
-      end
-
-      ::RGeo::Feature::MixinCollection::GLOBAL.for_type(::RGeo::Feature::Point).add(Mixin1)
-      ::RGeo::Feature::MixinCollection::GLOBAL.for_type(::RGeo::Feature::GeometryCollection).add(Mixin1)
-      ::RGeo::Feature::MixinCollection::GLOBAL.for_type(::RGeo::Feature::MultiCurve).add(Mixin2)
-
-      def test_basic_mixin_cartesian
-        factory_ = ::RGeo::Cartesian.simple_factory
-        assert_equal(::RGeo::Cartesian::PointImpl, factory_.point(1, 1).class)
-        assert(factory_.point(1, 1).class.include?(Mixin1))
-        assert(!factory_.point(1, 1).class.include?(Mixin2))
-        assert(factory_.point(1, 1).respond_to?(:mixin1_method))
-        assert(!factory_.point(1, 1).respond_to?(:mixin2_method))
-      end
-
-      def test_inherited_mixin_cartesian
-        factory_ = ::RGeo::Cartesian.simple_factory
-        assert(factory_.collection([]).class.include?(Mixin1))
-        assert(!factory_.collection([]).class.include?(Mixin2))
-        assert(factory_.collection([]).respond_to?(:mixin1_method))
-        assert(!factory_.collection([]).respond_to?(:mixin2_method))
-        assert(factory_.multi_line_string([]).class.include?(Mixin1))
-        assert(factory_.multi_line_string([]).class.include?(Mixin2))
-        assert(factory_.multi_line_string([]).respond_to?(:mixin1_method))
-        assert(factory_.multi_line_string([]).respond_to?(:mixin2_method))
-      end
-
-      if ::RGeo::Geos.capi_supported?
-
-        def test_basic_mixin_geos_capi
-          factory_ = ::RGeo::Geos.factory(native_interface: :capi)
-          assert_equal(::RGeo::Geos::CAPIPointImpl, factory_.point(1, 1).class)
-          assert(factory_.point(1, 1).class.include?(Mixin1))
-          assert(!factory_.point(1, 1).class.include?(Mixin2))
-          assert(factory_.point(1, 1).respond_to?(:mixin1_method))
-          assert(!factory_.point(1, 1).respond_to?(:mixin2_method))
-        end
-
-        def test_inherited_mixin_geos_capi
-          factory_ = ::RGeo::Geos.factory(native_interface: :capi)
-          assert(factory_.collection([]).class.include?(Mixin1))
-          assert(!factory_.collection([]).class.include?(Mixin2))
-          assert(factory_.collection([]).respond_to?(:mixin1_method))
-          assert(!factory_.collection([]).respond_to?(:mixin2_method))
-          assert(factory_.multi_line_string([]).class.include?(Mixin1))
-          assert(factory_.multi_line_string([]).class.include?(Mixin2))
-          assert(factory_.multi_line_string([]).respond_to?(:mixin1_method))
-          assert(factory_.multi_line_string([]).respond_to?(:mixin2_method))
-        end
-
-      end
-
-      if ::RGeo::Geos.ffi_supported?
-
-        def test_basic_mixin_geos_ffi
-          factory_ = ::RGeo::Geos.factory(native_interface: :ffi)
-          assert_equal(::RGeo::Geos::FFIPointImpl, factory_.point(1, 1).class)
-          assert(factory_.point(1, 1).class.include?(Mixin1))
-          assert(!factory_.point(1, 1).class.include?(Mixin2))
-          assert(factory_.point(1, 1).respond_to?(:mixin1_method))
-          assert(!factory_.point(1, 1).respond_to?(:mixin2_method))
-        end
-
-        def test_inherited_mixin_geos_ffi
-          factory_ = ::RGeo::Geos.factory(native_interface: :ffi)
-          assert(factory_.collection([]).class.include?(Mixin1))
-          assert(!factory_.collection([]).class.include?(Mixin2))
-          assert(factory_.collection([]).respond_to?(:mixin1_method))
-          assert(!factory_.collection([]).respond_to?(:mixin2_method))
-          assert(factory_.multi_line_string([]).class.include?(Mixin1))
-          assert(factory_.multi_line_string([]).class.include?(Mixin2))
-          assert(factory_.multi_line_string([]).respond_to?(:mixin1_method))
-          assert(factory_.multi_line_string([]).respond_to?(:mixin2_method))
-        end
-
-      end
-
-      def test_basic_mixin_spherical
-        factory_ = ::RGeo::Geographic.spherical_factory
-        assert_equal(::RGeo::Geographic::SphericalPointImpl, factory_.point(1, 1).class)
-        assert(factory_.point(1, 1).class.include?(Mixin1))
-        assert(!factory_.point(1, 1).class.include?(Mixin2))
-        assert(factory_.point(1, 1).respond_to?(:mixin1_method))
-        assert(!factory_.point(1, 1).respond_to?(:mixin2_method))
-      end
-
-      def test_inherited_mixin_spherical
-        factory_ = ::RGeo::Geographic.spherical_factory
-        assert(factory_.collection([]).class.include?(Mixin1))
-        assert(!factory_.collection([]).class.include?(Mixin2))
-        assert(factory_.collection([]).respond_to?(:mixin1_method))
-        assert(!factory_.collection([]).respond_to?(:mixin2_method))
-        assert(factory_.multi_line_string([]).class.include?(Mixin1))
-        assert(factory_.multi_line_string([]).class.include?(Mixin2))
-        assert(factory_.multi_line_string([]).respond_to?(:mixin1_method))
-        assert(factory_.multi_line_string([]).respond_to?(:mixin2_method))
-      end
-
-      def test_basic_mixin_simple_mercator
-        factory_ = ::RGeo::Geographic.simple_mercator_factory
-        assert_equal(::RGeo::Geographic::ProjectedPointImpl, factory_.point(1, 1).class)
-        assert(factory_.point(1, 1).class.include?(Mixin1))
-        assert(!factory_.point(1, 1).class.include?(Mixin2))
-        assert(factory_.point(1, 1).respond_to?(:mixin1_method))
-        assert(!factory_.point(1, 1).respond_to?(:mixin2_method))
-      end
-
-      def test_inherited_mixin_simple_mercator
-        factory_ = ::RGeo::Geographic.simple_mercator_factory
-        assert(factory_.collection([]).class.include?(Mixin1))
-        assert(!factory_.collection([]).class.include?(Mixin2))
-        assert(factory_.collection([]).respond_to?(:mixin1_method))
-        assert(!factory_.collection([]).respond_to?(:mixin2_method))
-        assert(factory_.multi_line_string([]).class.include?(Mixin1))
-        assert(factory_.multi_line_string([]).class.include?(Mixin2))
-        assert(factory_.multi_line_string([]).respond_to?(:mixin1_method))
-        assert(factory_.multi_line_string([]).respond_to?(:mixin2_method))
-      end
+class TestMixins < Test::Unit::TestCase # :nodoc:
+  module Mixin1  # :nodoc:
+    def mixin1_method
     end
+  end
+
+  module Mixin2  # :nodoc:
+    def mixin2_method
+    end
+  end
+
+  ::RGeo::Feature::MixinCollection::GLOBAL.for_type(::RGeo::Feature::Point).add(Mixin1)
+  ::RGeo::Feature::MixinCollection::GLOBAL.for_type(::RGeo::Feature::GeometryCollection).add(Mixin1)
+  ::RGeo::Feature::MixinCollection::GLOBAL.for_type(::RGeo::Feature::MultiCurve).add(Mixin2)
+
+  def test_basic_mixin_cartesian
+    factory_ = ::RGeo::Cartesian.simple_factory
+    assert_equal(::RGeo::Cartesian::PointImpl, factory_.point(1, 1).class)
+    assert(factory_.point(1, 1).class.include?(Mixin1))
+    assert(!factory_.point(1, 1).class.include?(Mixin2))
+    assert(factory_.point(1, 1).respond_to?(:mixin1_method))
+    assert(!factory_.point(1, 1).respond_to?(:mixin2_method))
+  end
+
+  def test_inherited_mixin_cartesian
+    factory_ = ::RGeo::Cartesian.simple_factory
+    assert(factory_.collection([]).class.include?(Mixin1))
+    assert(!factory_.collection([]).class.include?(Mixin2))
+    assert(factory_.collection([]).respond_to?(:mixin1_method))
+    assert(!factory_.collection([]).respond_to?(:mixin2_method))
+    assert(factory_.multi_line_string([]).class.include?(Mixin1))
+    assert(factory_.multi_line_string([]).class.include?(Mixin2))
+    assert(factory_.multi_line_string([]).respond_to?(:mixin1_method))
+    assert(factory_.multi_line_string([]).respond_to?(:mixin2_method))
+  end
+
+  if ::RGeo::Geos.capi_supported?
+
+    def test_basic_mixin_geos_capi
+      factory_ = ::RGeo::Geos.factory(native_interface: :capi)
+      assert_equal(::RGeo::Geos::CAPIPointImpl, factory_.point(1, 1).class)
+      assert(factory_.point(1, 1).class.include?(Mixin1))
+      assert(!factory_.point(1, 1).class.include?(Mixin2))
+      assert(factory_.point(1, 1).respond_to?(:mixin1_method))
+      assert(!factory_.point(1, 1).respond_to?(:mixin2_method))
+    end
+
+    def test_inherited_mixin_geos_capi
+      factory_ = ::RGeo::Geos.factory(native_interface: :capi)
+      assert(factory_.collection([]).class.include?(Mixin1))
+      assert(!factory_.collection([]).class.include?(Mixin2))
+      assert(factory_.collection([]).respond_to?(:mixin1_method))
+      assert(!factory_.collection([]).respond_to?(:mixin2_method))
+      assert(factory_.multi_line_string([]).class.include?(Mixin1))
+      assert(factory_.multi_line_string([]).class.include?(Mixin2))
+      assert(factory_.multi_line_string([]).respond_to?(:mixin1_method))
+      assert(factory_.multi_line_string([]).respond_to?(:mixin2_method))
+    end
+
+  end
+
+  if ::RGeo::Geos.ffi_supported?
+
+    def test_basic_mixin_geos_ffi
+      factory_ = ::RGeo::Geos.factory(native_interface: :ffi)
+      assert_equal(::RGeo::Geos::FFIPointImpl, factory_.point(1, 1).class)
+      assert(factory_.point(1, 1).class.include?(Mixin1))
+      assert(!factory_.point(1, 1).class.include?(Mixin2))
+      assert(factory_.point(1, 1).respond_to?(:mixin1_method))
+      assert(!factory_.point(1, 1).respond_to?(:mixin2_method))
+    end
+
+    def test_inherited_mixin_geos_ffi
+      factory_ = ::RGeo::Geos.factory(native_interface: :ffi)
+      assert(factory_.collection([]).class.include?(Mixin1))
+      assert(!factory_.collection([]).class.include?(Mixin2))
+      assert(factory_.collection([]).respond_to?(:mixin1_method))
+      assert(!factory_.collection([]).respond_to?(:mixin2_method))
+      assert(factory_.multi_line_string([]).class.include?(Mixin1))
+      assert(factory_.multi_line_string([]).class.include?(Mixin2))
+      assert(factory_.multi_line_string([]).respond_to?(:mixin1_method))
+      assert(factory_.multi_line_string([]).respond_to?(:mixin2_method))
+    end
+
+  end
+
+  def test_basic_mixin_spherical
+    factory_ = ::RGeo::Geographic.spherical_factory
+    assert_equal(::RGeo::Geographic::SphericalPointImpl, factory_.point(1, 1).class)
+    assert(factory_.point(1, 1).class.include?(Mixin1))
+    assert(!factory_.point(1, 1).class.include?(Mixin2))
+    assert(factory_.point(1, 1).respond_to?(:mixin1_method))
+    assert(!factory_.point(1, 1).respond_to?(:mixin2_method))
+  end
+
+  def test_inherited_mixin_spherical
+    factory_ = ::RGeo::Geographic.spherical_factory
+    assert(factory_.collection([]).class.include?(Mixin1))
+    assert(!factory_.collection([]).class.include?(Mixin2))
+    assert(factory_.collection([]).respond_to?(:mixin1_method))
+    assert(!factory_.collection([]).respond_to?(:mixin2_method))
+    assert(factory_.multi_line_string([]).class.include?(Mixin1))
+    assert(factory_.multi_line_string([]).class.include?(Mixin2))
+    assert(factory_.multi_line_string([]).respond_to?(:mixin1_method))
+    assert(factory_.multi_line_string([]).respond_to?(:mixin2_method))
+  end
+
+  def test_basic_mixin_simple_mercator
+    factory_ = ::RGeo::Geographic.simple_mercator_factory
+    assert_equal(::RGeo::Geographic::ProjectedPointImpl, factory_.point(1, 1).class)
+    assert(factory_.point(1, 1).class.include?(Mixin1))
+    assert(!factory_.point(1, 1).class.include?(Mixin2))
+    assert(factory_.point(1, 1).respond_to?(:mixin1_method))
+    assert(!factory_.point(1, 1).respond_to?(:mixin2_method))
+  end
+
+  def test_inherited_mixin_simple_mercator
+    factory_ = ::RGeo::Geographic.simple_mercator_factory
+    assert(factory_.collection([]).class.include?(Mixin1))
+    assert(!factory_.collection([]).class.include?(Mixin2))
+    assert(factory_.collection([]).respond_to?(:mixin1_method))
+    assert(!factory_.collection([]).respond_to?(:mixin2_method))
+    assert(factory_.multi_line_string([]).class.include?(Mixin1))
+    assert(factory_.multi_line_string([]).class.include?(Mixin2))
+    assert(factory_.multi_line_string([]).respond_to?(:mixin1_method))
+    assert(factory_.multi_line_string([]).respond_to?(:mixin2_method))
   end
 end
