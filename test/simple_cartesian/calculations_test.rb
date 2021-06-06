ddeecc03239f900c3d262ea6e3eebd078a91218c
Keith Doggett <keith.doggett887@gmail.com>
2021-06-06 18:53:03 -0400

    Added Segment#segment_intersection to get points from intersections, created SweeplineIntersector class to compute intersections on groups of segments.
    Segment#segment_intersection sligtly modifies the code from intersects_segment? and actually computes the intersection point (or nil if none).
    Edge cases for colinear segments are handled by return a single point from the intersection.
    intersects_segment? was modified to just check if segment_intersection is not nil.
    
    A SweeplineIntersector class was added that uses a basic sweepline algorithm to compute the intersections in a set of segments.
    This should be faster or as fast as the current validate_geometry intersection algorithm being used (more testing needed), since
    it only compares segments where their y-range includes the y value of the event the sweepline is handling.
    There are options to return all intersections or proper intersections that will filter out the connections in LineStrings.
    
    In the future, it is possible to make a Sweepline class and have the SweeplineIntersector inherit from it. This would allow
    us to use the sweepline more generically in other applications (some polygon clipping algos use a sweepline).

--- test/simple_cartesian/calculations_test.rb
+++ test/simple_cartesian/calculations_test.rb
@@ -28,6 +28,21 @@ class CartesianCalculationsTest < Minitest::Test # :nodoc:
     @parallel_rising_seg = RGeo::Cartesian::Segment.new(@point6, @point7)
     @steep_rising_seg = RGeo::Cartesian::Segment.new(@point6, @point4)
     @degenerate_seg = RGeo::Cartesian::Segment.new(@point5, @point5)
+
+    # sweepline tests
+    @point9 = @factory.point(0, 0)
+    @point10 = @factory.point(0, 1)
+    @point11 = @factory.point(-0.5, 0.5)
+    @point12 = @factory.point(0.5, 0.5)
+    @point13 = @factory.point(0, 0.5)
+    @point14 = @factory.point(0.5, 0.6)
+    @point15 = @factory.point(0, 0.6)
+
+    @li_seg1 = RGeo::Cartesian::Segment.new(@point9, @point10)
+    @li_seg2 = RGeo::Cartesian::Segment.new(@point11, @point12)
+    @li_seg3 = RGeo::Cartesian::Segment.new(@point12, @point13)
+    @li_seg4 = RGeo::Cartesian::Segment.new(@point14, @point13)
+    @li_seg5 = RGeo::Cartesian::Segment.new(@point14, @point15)
   end
 
   def assert_close_enough(v1, v2)
@@ -90,5 +105,101 @@ class CartesianCalculationsTest < Minitest::Test # :nodoc:
   def test_segment_intersects_endpoints
     assert(@horiz_seg.intersects_segment?(@vert_seg))
     assert(@vert_seg.intersects_segment?(@horiz_seg))
+  end
+
+  def test_segment_intersection_parallel
+    assert_equal(@point5, @long_rising_seg.segment_intersection(@long_rising_seg))
+    assert_nil(@long_rising_seg.segment_intersection(@parallel_rising_seg))
+    assert_equal(@point1, @long_rising_seg.segment_intersection(@short_rising_seg))
+    assert_equal(@point1, @touching_collinear_rising_seg.segment_intersection(@long_rising_seg))
+  end
+
+  def test_segment_intersection_basic
+    int1 = @factory.point(1, 3)
+    assert_equal(int1, @long_rising_seg.segment_intersection(@steep_rising_seg))
+
+    int2 = @factory.point(0, 0.5)
+    assert_equal(int2, @li_seg1.segment_intersection(@li_seg2))
+
+    assert_nil(@short_rising_seg.segment_intersection(@steep_rising_seg))
+  end
+
+  def test_segment_intersection_endpoints
+    assert_equal(@point1, @horiz_seg.segment_intersection(@vert_seg))
+    assert_equal(@point1, @vert_seg.segment_intersection(@horiz_seg))
+  end
+
+  def test_sweepline_create_events
+    segs = [@li_seg1, @li_seg2]
+    li = RGeo::Cartesian::SweeplineIntersector.new(segs)
+
+    assert_equal(4, li.events.size)
+    events = li.events
+    assert_equal(@point10, events[0].point)
+    assert_equal(@point11, events[1].point)
+    assert_equal(@point12, events[2].point)
+    assert_equal(@point9, events[3].point)
+
+    assert_equal(@li_seg1, events[0].segment)
+    assert_equal(@li_seg2, events[1].segment)
+    assert_equal(@li_seg2, events[2].segment)
+    assert_equal(@li_seg1, events[3].segment)
+
+    assert(events[0].is_start)
+    assert(events[1].is_start)
+    refute(events[2].is_start)
+    refute(events[3].is_start)
+  end
+
+  def test_sweepline_intersections_crossing_lines
+    segs1 = [@li_seg1, @li_seg2]
+    li = RGeo::Cartesian::SweeplineIntersector.new(segs1)
+    int_pt = @factory.point(0, 0.5)
+
+    assert_equal(1, li.intersections.size)
+
+    intersection = li.intersections.first
+    expected = RGeo::Cartesian::SweeplineIntersector::Intersection.new(int_pt, @li_seg2, @li_seg1)
+    assert_equal(expected, intersection)
+  end
+
+  def test_sweepline_intersections_converging_lines
+    segs = [@li_seg1, @li_seg3, @li_seg4]
+    li = RGeo::Cartesian::SweeplineIntersector.new(segs)
+    int_pt = @factory.point(0, 0.5)
+
+    assert_equal(3, li.intersections.size)
+    li.intersections.each do |int|
+      assert_equal(int_pt, int.point)
+    end
+  end
+
+  def test_sweepline_intersections_crossing_lines_and_touching_line
+    segs = [@li_seg1, @li_seg2, @li_seg5]
+    li = RGeo::Cartesian::SweeplineIntersector.new(segs)
+    int_pt1 = @factory.point(0, 0.6)
+    int_pt2 = @factory.point(0, 0.5)
+
+    assert_equal(2, li.intersections.size)
+
+    expected1 = RGeo::Cartesian::SweeplineIntersector::Intersection.new(int_pt1, @li_seg5, @li_seg1)
+    expected2 = RGeo::Cartesian::SweeplineIntersector::Intersection.new(int_pt2, @li_seg2, @li_seg1)
+    assert_equal([expected1, expected2], li.intersections)
+  end
+
+  def test_sweepline_proper_intersections
+    square_pts = [@factory.point(0, 0), @factory.point(0, 1), @factory.point(1, 1), @factory.point(1, 0), @factory.point(0, 0)]
+    square = @factory.line_string(square_pts)
+
+    hourglass_pts = [@factory.point(0, 0), @factory.point(0, 1), @factory.point(1, 0), @factory.point(1, 1), @factory.point(0, 0)]
+    hourglass = @factory.line_string(hourglass_pts)
+
+    square_li = RGeo::Cartesian::SweeplineIntersector.new(square.segments)
+    assert_equal(0, square_li.proper_intersections.size)
+
+    hourglass_li = RGeo::Cartesian::SweeplineIntersector.new(hourglass.segments)
+    assert_equal(1, hourglass_li.proper_intersections.size)
+    intersections = hourglass_li.proper_intersections
+    assert_equal(@factory.point(0.5, 0.5), intersections.first.point)
   end
 end
