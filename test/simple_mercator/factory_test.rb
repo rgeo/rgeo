250382f35a5309cc93c0ec0fcd6b5d974dee50c6
thestelz <dev@andrew24.com>
2021-11-10 12:11:48 -0500

    feat: add simple factory test and revert gitignore update
    * Add a simple factory test for :uses_lenient_assertions on
    `simple_mercator_factory`.
    
    * Remove `.idea` from `.gitignore`.
    
    updates rgeo/rgeo#277

--- test/simple_mercator/factory_test.rb
+++ test/simple_mercator/factory_test.rb
@@ -15,4 +15,10 @@ class MercatorFactoryTest < Minitest::Test # :nodoc:
     @factory = RGeo::Geographic.simple_mercator_factory
     @srid = 4326
   end
+
+  def test_has_uses_lenient_assertions
+    factory = RGeo::Geographic.simple_mercator_factory(uses_lenient_assertions: true)
+
+    assert_equal(true, factory.property(:uses_lenient_assertions))
+  end
 end
