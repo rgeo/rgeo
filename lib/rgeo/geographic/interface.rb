165bf780cbb88c95a477af804521447d7e90f251
Keith Doggett <keith.doggett887@gmail.com>
2022-09-21 09:51:52 -0400

    change interface of *Projector classes to rely on coord_sys

--- lib/rgeo/geographic/interface.rb
+++ lib/rgeo/geographic/interface.rb
@@ -345,8 +345,7 @@ module RGeo
             has_m_coordinate: opts[:has_m_coordinate],
             wkt_parser: opts[:wkt_parser], wkt_generator: opts[:wkt_generator],
             wkb_parser: opts[:wkb_parser], wkb_generator: opts[:wkb_generator])
-          projector = Geographic::Proj4Projector.create_from_proj4(factory,
-            nil,
+          projector = Geographic::Proj4Projector.create_from_opts(factory,
             srid: projection_srid,
             coord_sys: projection_coord_sys,
             buffer_resolution: opts[:buffer_resolution],
--- lib/rgeo/geographic/simple_mercator_projector.rb
+++ lib/rgeo/geographic/simple_mercator_projector.rb
@@ -93,7 +93,7 @@ module RGeo
 
       def self._coordsys_3857 # :nodoc:
         unless defined?(@coordsys_3857)
-          @coordsys_3857 = CoordSys::CS.create_from_wkt('PROJCS["Popular Visualisation CRS / Mercator",GEOGCS["Popular Visualisation CRS",DATUM["Popular_Visualisation_Datum",SPHEROID["Popular Visualisation Sphere",6378137,0,AUTHORITY["EPSG","7059"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY["EPSG","6055"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.01745329251994328,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4055"]],UNIT["metre",1,AUTHORITY["EPSG","9001"]],PROJECTION["Mercator_1SP"],PARAMETER["central_meridian",0],PARAMETER["scale_factor",1],PARAMETER["false_easting",0],PARAMETER["false_northing",0],AUTHORITY["EPSG","3785"],AXIS["X",EAST],AXIS["Y",NORTH]]')
+          @coordsys_3857 = CoordSys::CONFIG.default_coord_sys_class.create(3785)
         end
         @coordsys_3857
       end
