0fd218e470f4c7db19f81bf0f41cbaa61f7f246e
AJ <dev@andrew24.com>
2022-11-02 17:02:54 -0400

    feat: update cartesian factor caller from feedback
    updates rgeo/rgeo#336, rgeo/rgeo#338

--- lib/rgeo/cartesian/factory.rb
+++ lib/rgeo/cartesian/factory.rb
@@ -109,10 +109,9 @@ module RGeo
       end
 
       def marshal_load(data) # :nodoc:
-        coord_sys =
-          if (coord_sys_data = data["cs"])
-            CoordSys::CONFIG.default_coord_sys_class.create_from_wkt(coord_sys_data)
-          end
+        cs_class = CoordSys::CONFIG.default_coord_sys_class
+        coord_sys = data["cs"]&.then { |cs| cs_class.create_from_wkt(cs) }
+
         initialize(
           has_z_coordinate: data["hasz"],
           has_m_coordinate: data["hasm"],
