09bb915f51ee1395148348e871f11b5c59aeab70
Keith Doggett <keith.doggett887@gmail.com>
2023-08-24 16:56:44 -0400

    add version conditional to test_invalid_polygon_duplicate_rings

--- test/common/validity_tests.rb
+++ test/common/validity_tests.rb
@@ -224,7 +224,12 @@ module RGeo
 
         def test_invalid_polygon_duplicate_rings
           poly = @factory.polygon(big_square, [little_square, little_square])
-          assert_equal(RGeo::Error::SELF_INTERSECTION, poly.invalid_reason)
+
+          if geos_version_match("3.10.0")
+            assert_equal(RGeo::Error::SELF_INTERSECTION, poly.invalid_reason)
+          else
+            assert_equal(RGeo::Error::DUPLICATE_RINGS, poly.invalid_reason)
+          end
           assert_equal(false, poly.valid?)
         end
 
