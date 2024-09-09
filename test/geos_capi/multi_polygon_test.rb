cff3070d37e2dd5be360d550e4c7319854dda648
Mikhail Varabyou <varaby_m@modulotech.fr>
2024-09-09 17:37:57 +0200

    update branch. remove constants from tests.

--- test/geos_capi/multi_polygon_test.rb
+++ test/geos_capi/multi_polygon_test.rb
@@ -38,54 +38,64 @@ class GeosMultiPolygonTest < Minitest::Test # :nodoc:
     assert_equal expected, input.polygonize
   end
 
-  INVALID_MULTIPOLYGON = "MULTIPOLYGON (((0 1,2 1,2 2,0 2,0 1)), ((1 0,3 0,3 3,1 3,1 0)))"
-  LINEWORK_MULTIPOLYGON = "MULTIPOLYGON (((1 2, 1 1, 0 1, 0 2, 1 2)), " \
-                          "((1 3, 3 3, 3 0, 1 0, 1 1, 2 1, 2 2, 1 2, 1 3)))"
-  JOINED_POLYGON = "POLYGON ((0 2, 1 2, 1 3, 3 3, 3 0, 1 0, 1 1, 0 1, 0 2))"
-
   # default make_valid uses linework method and intersection is removed
   def test_make_valid
-    multipolygon = @factory.parse_wkt(INVALID_MULTIPOLYGON)
-    expected = @factory.parse_wkt(LINEWORK_MULTIPOLYGON)
+    multipolygon = @factory.parse_wkt(invalid_multipolygon)
+    expected = @factory.parse_wkt(linework_multipolygon)
 
     assert_equal expected, multipolygon.make_valid
   end
 
   # linework is default so result is same as without params for multipolygon
   def test_make_valid_method_linework
-    multipolygon = @factory.parse_wkt(INVALID_MULTIPOLYGON)
-    expected = @factory.parse_wkt(LINEWORK_MULTIPOLYGON)
+    multipolygon = @factory.parse_wkt(invalid_multipolygon)
+    expected = @factory.parse_wkt(linework_multipolygon)
 
     assert_equal expected, multipolygon.make_valid(method: :linework)
   end
 
   # make_valid with method structure joins intersected polygons
   def test_make_valid_method_structure
-    multipolygon = @factory.parse_wkt(INVALID_MULTIPOLYGON)
-    expected = @factory.parse_wkt(JOINED_POLYGON)
+    multipolygon = @factory.parse_wkt(invalid_multipolygon)
+    expected = @factory.parse_wkt(joined_polygon)
 
     assert_equal expected, multipolygon.make_valid(method: :structure)
   end
 
   def test_make_valid_method_structure_string
-    multipolygon = @factory.parse_wkt(INVALID_MULTIPOLYGON)
-    expected = @factory.parse_wkt(JOINED_POLYGON)
+    multipolygon = @factory.parse_wkt(invalid_multipolygon)
+    expected = @factory.parse_wkt(joined_polygon)
 
     assert_equal expected, multipolygon.make_valid(method: "structure")
   end
 
   def test_make_valid_method_unknown
     assert_raises(ArgumentError) do
-      @factory.parse_wkt(INVALID_MULTIPOLYGON).make_valid(method: :some_method)
+      @factory.parse_wkt(invalid_multipolygon).make_valid(method: :some_method)
     end
   end
 
   # make_valid with method structure and keep_collapsed has no effect - works as method=structure
   def test_make_valid_method_structure_keep_collapsed
-    multipolygon = @factory.parse_wkt(INVALID_MULTIPOLYGON)
-    expected = @factory.parse_wkt(JOINED_POLYGON)
+    multipolygon = @factory.parse_wkt(invalid_multipolygon)
+    expected = @factory.parse_wkt(joined_polygon)
 
     assert_equal expected, multipolygon.make_valid(method: :structure, keep_collapsed: false)
     assert_equal expected, multipolygon.make_valid(method: :structure, keep_collapsed: true)
+  end
+
+  private
+
+  def invalid_multipolygon
+    "MULTIPOLYGON (((0 1,2 1,2 2,0 2,0 1)), ((1 0,3 0,3 3,1 3,1 0)))"
+  end
+
+  def linework_multipolygon
+    "MULTIPOLYGON (((1 2, 1 1, 0 1, 0 2, 1 2)), " \
+      "((1 3, 3 3, 3 0, 1 0, 1 1, 2 1, 2 2, 1 2, 1 3)))"
+  end
+
+  def joined_polygon
+    "POLYGON ((0 2, 1 2, 1 3, 3 3, 3 0, 1 0, 1 1, 0 1, 0 2))"
   end
 end
